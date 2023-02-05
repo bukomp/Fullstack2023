b7a3832661e8e0e23e5f3d3216403341090c5beb
Edvard <edvard@shalaev.com>
2023-02-05 22:53:54 +0300

    feat: 1.10 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,31 +1,39 @@
 import { useState } from 'react'
 
+const Button = ({ state, stateUpdateFunction, status }) => <button onClick={() => stateUpdateFunction(++state)}>{status}</button>
 
-const Statistic = ({ statiticName, statitic }) => <p>{statiticName} {statitic}</p>
+const Feedback = ({ good, neutral, bad, setGood, setNeutral, setBad }) => (
+  <div>
+    <h1>give feedback</h1>
+    <Button state={good} stateUpdateFunction={setGood} status={"good"} />
+    <Button state={neutral} stateUpdateFunction={setNeutral} status={"neutral"} />
+    <Button state={bad} stateUpdateFunction={setBad} status={"bad"} />
+  </div>
+)
+
+const StatisticLine = ({ statiticName, value }) => <p>{statiticName} {value}</p>
 
 const Statistics = ({ good, neutral, bad }) => {
   const positive = ((good / (neutral + bad)) * 100)
 
   return (
-
     <div>
       <h1>statistics</h1>
 
       {
         good + neutral + bad ?
           <>
-            <Statistic statiticName={"good"} statitic={good} />
-            <Statistic statiticName={"neutral"} statitic={neutral} />
-            <Statistic statiticName={"bad"} statitic={bad} />
-            <Statistic statiticName={"all"} statitic={good + neutral + bad} />
-            <Statistic statiticName={"average"} statitic={(good - bad) / 3} />
-            <Statistic statiticName={"positive"} statitic={(!isNaN(positive) && isFinite(positive) ? positive : 0) + "%"} />
+            <StatisticLine statiticName={"good"} value={good} />
+            <StatisticLine statiticName={"neutral"} value={neutral} />
+            <StatisticLine statiticName={"bad"} value={bad} />
+            <StatisticLine statiticName={"all"} value={good + neutral + bad} />
+            <StatisticLine statiticName={"average"} value={(good - bad) / 3} />
+            <StatisticLine statiticName={"positive"} value={(!isNaN(positive) && isFinite(positive) ? positive : 0) + "%"} />
           </>
           :
           <p>No feedback given</p>
       }
     </div>
-
   )
 }
 
@@ -34,23 +42,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const Feedback = () => (
-    <div>
-      <h1>give feedback</h1>
-      <Button state={good} stateUpdateFunction={setGood} status={"good"} />
-      <Button state={neutral} stateUpdateFunction={setNeutral} status={"neutral"} />
-      <Button state={bad} stateUpdateFunction={setBad} status={"bad"} />
-    </div>
-  )
-
-
-
-  const Button = ({ state, stateUpdateFunction, status }) => <button onClick={() => stateUpdateFunction(++state)}>{status}</button>
-
-
   return (
     <div>
-      <Feedback />
+      <Feedback
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        setGood={setGood}
+        setNeutral={setNeutral}
+        setBad={setBad}
+      />
       <Statistics
         good={good}
         neutral={neutral}
