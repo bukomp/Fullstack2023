72fc81881bb679a5279795285bfe5a2b8bf04c9f
Edvard <edvard@shalaev.com>
2023-06-13 15:07:53 +0300

    feat: 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,16 +6,25 @@ const app = express();
 const PORT = 3001;
 
 app.get('/api/persons/:id', (req, res) => {
-  if(0 >= +req.params.id > phonebook.length){
+  if(Number(req.params.id) <= 0 || Number(req.params.id) > phonebook.length){
     return res.status(404).end()
   } else {
     return res.json(phonebook[+req.params.id-1]);
   }
 });
 
+app.delete('/api/persons/:id', (req, res) => {
+  if(Number(req.params.id) <= 0 || Number(req.params.id) > phonebook.length){
+    return res.status(404).end()
+  } else {
+    phonebook.splice(+req.params.id-1, 1)
+    return res.json(phonebook);
+  }
+});
+
 app.get('/api/persons', (req, res) => {
   return res.json(phonebook);
-});
+}); 
 
 
 
