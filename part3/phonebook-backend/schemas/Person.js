2b43db897e88159c01ad031186c615f75c674a4e
Edvard <edvard@shalaev.com>
2023-06-14 18:51:12 +0300

    feat: 3.20 & 3.21

--- part3/phonebook-backend/schemas/Person.js
+++ part3/phonebook-backend/schemas/Person.js
@@ -1,11 +1,24 @@
 const mongoose = require('../mongo.db')
 
+const phoneValidator = {
+  validator: function(value) {
+    const phoneRegex = /^\d{2,3}-\d+$/;
+    return phoneRegex.test(value);
+  },
+  message: 'Invalid phone number format. Please provide a valid phone number with the format xx(x)-xxxxxxx(x).',
+};
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 3
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: phoneValidator,
+    minlength: 8
+  },
 })
 
 personSchema.set('toJSON', {
