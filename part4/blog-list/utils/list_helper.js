91fcaed5263da8984232da03e951d55d17dd32a7
Edvard <edvard@shalaev.com>
2023-06-15 15:51:19 +0300

    feat: 4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0
+  );
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
