6f4c2a9a615c8eea8b005ef01c896c2ec18cd860
Edvard <edvard@shalaev.com>
2023-07-11 20:43:41 +0300

    feat: 4.16

--- part4/blog-list/controllers/users.controller.js
+++ part4/blog-list/controllers/users.controller.js
@@ -5,6 +5,23 @@ const User = require("../models/user.schema");
 usersRouter.post("/user", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (!username || !password) {
+    return response
+      .status(400)
+      .send({ error: "username and password must be provided" });
+  } else if (username.length < 3 || password.length < 3) {
+    return response
+      .status(400)
+      .send({
+        error: "username and password must be at least 3 characters long",
+      });
+  }
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).send({ error: "username already exists" });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
@@ -14,9 +31,12 @@ usersRouter.post("/user", async (request, response) => {
     passwordHash,
   });
 
-  const savedUser = await user.save();
-
-  response.status(201).json(savedUser);
+  try {
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+  } catch (error) {
+    response.status(500).send({ error: "could not save user" });
+  }
 });
 
 usersRouter.get("/user/all", async (request, response) => {
--- part4/blog-list/tests/user_api.test.js
+++ part4/blog-list/tests/user_api.test.js
@@ -27,6 +27,56 @@ test("1 user is returned from DB", async () => {
   expect(userList.body.length).toStrictEqual(1);
 });
 
+test("user is not created if username is not unique", async () => {
+  await User.deleteMany();
+
+  await api
+    .post("/api/user")
+    .send({
+      name: "name1",
+      username: "user",
+      password: "password1",
+    })
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const newUser = await api
+    .post("/api/user")
+    .send({
+      name: "name2",
+      username: "user",
+      password: "password2",
+    })
+    .expect(400)
+    .expect("Content-Type", /application\/json/);
+
+  expect(newUser.body.error).toEqual("username already exists");
+
+  const usersAtEnd = await api.get("/api/user/all").expect(200);
+  expect(usersAtEnd.body.length).toStrictEqual(1);
+});
+
+test("user is not created if username or password are less than 3 characters long", async () => {
+  await User.deleteMany();
+
+  const newUser = await api
+    .post("/api/user")
+    .send({
+      name: "name",
+      username: "us",
+      password: "pass",
+    })
+    .expect(400)
+    .expect("Content-Type", /application\/json/);
+
+  expect(newUser.body.error).toEqual(
+    "username and password must be at least 3 characters long"
+  );
+
+  const usersAtEnd = await api.get("/api/user/all").expect(200);
+  expect(usersAtEnd.body.length).toStrictEqual(0);
+});
+
 afterAll(async () => {
   await User.deleteMany();
   await mongoose.connection.close();
