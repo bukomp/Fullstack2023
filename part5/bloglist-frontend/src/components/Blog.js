d94e89cd4dc19a4885594de57b0c3a5851a533a8
Edvard <edvard@shalaev.com>
2023-07-15 12:26:40 +0300

    feat: 5.7 & 5.8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleBlogData = (event) => {
+    event.preventDefault();
+    setToggle(!toggle);
+  };
+
+  if (!toggle) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleBlogData}>view</button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleBlogData}>hide</button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
