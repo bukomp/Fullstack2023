5dc64d5e0ccbb0eca2a00ef07da2dcb6ee1b37b5
Edvard <edvard@shalaev.com>
2023-08-24 11:09:03 +0300

    feat: 6.12

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,8 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import anecdoteReducer from './reducers/anecdoteReducer';
 import filterReducer from './reducers/filterReducer';
+import notificationReducer from './reducers/notificationReducer';
 
 const reducer = {
+  notification: notificationReducer,
   anecdotes: anecdoteReducer,
   filter: filterReducer,
 };
